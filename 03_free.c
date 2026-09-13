74e82838d0219f4f3d779ab618819a395e6ce50e
Subham Kar <karsubham669@gmail.com>
2026-09-13 10:08:18 +0530

    Add Chapter 11 - dynamic memory allocation (malloc, calloc, free, realloc)

---
+++ 03_free.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    int*ptr;
+    scanf("%d",&n);
+    //int arr [n] // not allowed in c
+    ptr = (int*)calloc(n, sizeof(int));
+    ptr[0] = 3;
+    ptr [1] = 6;
+    printf("%d %d\n", ptr[0] , ptr[1]);
+    free(ptr);
+    return 0;
+}
