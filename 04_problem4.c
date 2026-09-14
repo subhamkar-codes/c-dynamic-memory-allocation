046556d979c8e676589bc5887aaf967a41e00b3e
Subham Kar <karsubham669@gmail.com>
2026-09-14 10:31:41 +0530

    Add Chapter 11 practice set - dynamic memory allocation Q4

---
+++ 04_problem4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n = 5;
+    int *ptr;
+    // printf("Enter the number of Elements\n");
+    // scanf("%d",&n);
+    ptr = (int *)malloc(n* sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("Thr Array is:\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", ptr[i]);
+    }
+
+    n = 10;
+    ptr = (int *)realloc(ptr, 10 * sizeof(int));
+    printf("Enter New Array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    printf("Thr Array is:\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", ptr[i]);
+    }
+    return 0;
+}
